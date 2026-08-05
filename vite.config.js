574ddbad6196d4e121b9c2631ac3ee6afab2df89
camilomurielph <149724435+camilomurielph@users.noreply.github.com>
2026-08-04 21:14:20 -0500

    Eliminar vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/finanzas/', // ← cambia por el nombre exacto de tu repositorio
-  server: { port: 3000 }
-})
